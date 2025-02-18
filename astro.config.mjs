81875a387554005d0dafda3c7bee4f5540c651cc
GUILHERME KELLER <121647297+guilh00009@users.noreply.github.com>
2025-02-18 18:57:18 -0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,13 @@ export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
   adapter: netlify(),
+  server: {
+    host: true,
+    port: 3000,
+  },
+  vite: {
+    server: {
+      allowedHosts: ['castle.nico.re', 'localhost'],
+    },
+  },
 });
